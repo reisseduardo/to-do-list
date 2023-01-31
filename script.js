4f1e2d133479d0a525eae4267e4956fbe0937f2d
Eduardo Reis <reisdasilva.eduardo@gmail.com>
2023-01-31 10:26:48 -0300

    adicionando tarefas dinamicamente

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let input = document.getElementById("userInput");
-let button = document.getElementById("addTask");
+const input = document.querySelector("#userInput");
+const addTaskButton = document.querySelector("#addTask");
+const userTask = document.querySelector(".userTask");
+
+
+const validateInput = () => input.value.trim().length > 0;
+const handleAddTask = () => {
+    const inputIsValid = validateInput();    
+
+    if (!inputIsValid){
+        return alert("Digite sua tarefa.");
+    }
+    const taskItem = document.createElement("ul");
+    taskItem.classList.add("task-item");
+
+    const userText = document.createElement("li");
+    userText.innerText = input.value;
+
+    const deleteItem = document.createElement("button");
+    const label = document.createTextNode("Remover");
+    deleteItem.appendChild(label)
+    deleteItem.classList.add("del")
+
+    taskItem.appendChild(userText);
+    taskItem.appendChild(deleteItem);
+
+    userTask.appendChild(taskItem);
+
+}
+
+addTaskButton.addEventListener("click", () => handleAddTask());
+
